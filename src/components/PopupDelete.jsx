b6626937fb16bada0e8e5b840862ee23135dd3d8
whtevr <moskovm878@gmail.com>
2025-07-08 10:31:17 +0700

    Membuat websitenya menjadi responsif terhadap smartphone dan tablet

--- src/components/PopupDelete.jsx
+++ src/components/PopupDelete.jsx
@@ -1,21 +1,21 @@
 const PopupDelete = ({ onConfirm, onCancel }) => {
   return (        
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-xl shadow-lg p-6 w-[90%] max-w-md">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/70">
+      <div className="bg-white rounded-[2rem] shadow-lg p-6 w-[90%] max-w-md">
         <h2 className="text-lg font-bold mb-4 text-gray-800">Konfirmasi Hapus</h2>
-        <p className="text-gray-600 mb-6">
+        <p className="text-gray-600 mb-6 sm:text-[1rem] text-[0.8rem]">
           Apakah kamu yakin ingin menghapus produk ini? Tindakan ini tidak dapat dibatalkan.
         </p>
         <div className="flex justify-end gap-3">
           <button
             onClick={onCancel}
-            className="px-4 py-2 border border-gray-300 rounded hover:bg-gray-100"
+            className="sm:text-[1rem] text-[0.8rem] px-4 py-2 border border-gray-300 rounded-[0.8rem] hover:bg-gray-100"
           >
             Batal
           </button>
           <button
             onClick={onConfirm}
-            className="px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded"
+            className="sm:text-[1rem] text-[0.8rem] px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-[0.8rem]"
           >
             Hapus
           </button>
